3b9ecf4914ef102a1f837ee038caa2be43039ba3
Vladimir Ovcharov <vladimir.1527@gmail.com>
2014-10-15 18:32:47 +0300

    all chapters added

---
+++ src/main/java/Chap01/Bank/BankAccount.java
@@ -0,0 +1,26 @@
+package algorithms.Chap01.Bank;
+
+class BankAccount
+{
+    private double balance;                   // account balance
+
+    public BankAccount(double openingBalance) // constructor
+    {
+        balance = openingBalance;
+    }
+
+    public void deposit(double amount)        // makes deposit
+    {
+        balance = balance + amount;
+    }
+
+    public void withdraw(double amount)       // makes withdrawal
+    {
+        balance = balance - amount;
+    }
+
+    public void display()                     // displays balance
+    {
+        System.out.println("balance=" + balance);
+    }
+}  // end class BankAccount
---
+++ src/main/java/Chap01/Bank/BankApp.java
@@ -0,0 +1,18 @@
+package algorithms.Chap01.Bank;
+
+class BankApp
+{
+    public static void main(String[] args)
+    {
+        BankAccount ba1 = new BankAccount(100.00); // create acct
+
+        System.out.print("Before transactions, ");
+        ba1.display();                         // display balance
+
+        ba1.deposit(74.35);                    // make deposit
+        ba1.withdraw(20.00);                   // make withdrawal
+
+        System.out.print("After transactions, ");
+        ba1.display();                         // display balance
+    }  // end main()
+}  // end class BankApp
